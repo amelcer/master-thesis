ca723cf7420d5a559323fc459bdfbd8366378841
Andrzej Melcer <an.melcer@gmail.com>
2024-08-21 02:22:15 +0200

    Refactor python

--- python/app/app.py
+++ python/app/app.py
@@ -6,25 +6,40 @@ app = Flask(__name__)
 @app.route('/people', methods=['GET'])
 def get_people():
     from_line = request.args.get('from', default=0, type=int)
-    to_line = request.args.get('to', type=int)
+    to_line = request.args.get('to', default=None, type=int)
 
-    if to_line is None:
-        to_line = float('inf')  # Ustawia to_line na bardzo dużą liczbę, aby uwzględnić wszystkie linie do końca pliku
-
-    if from_line > to_line:
-        return jsonify({'message': "'from' value should be lower than 'to' value"}), 400
+    if to_line is None or to_line < from_line:
+        to_line = float('inf') 
 
     people = []
-    with open('/data/data.csv', 'r') as file:
-        reader = csv.DictReader(file)
-        for line_num, row in enumerate(reader):
-            if line_num < from_line:
-                continue
-            if line_num > to_line:
-                break
-            people.append(row)
-    
-    people.sort(key=lambda x: x['lastName'])
+    file_path = '/data/data.csv'
 
-    return jsonify({'count': 500000, 'data': people})
+    try:
+        with open(file_path, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            headers = reader.fieldnames
 
+            for line_number, row in enumerate(reader, start=1):
+                if line_number < from_line:  
+                    continue
+                if line_number > to_line:
+                    break
+
+                person = {
+                    'id': row[headers[0]],
+                    'name': row[headers[1]],
+                    'lastName': row[headers[2]],
+                    'birthday': row[headers[3]],
+                    'email': row[headers[4]],
+                }
+                people.append(person)
+
+        people.sort(key=lambda p: p['lastName'])
+
+        return jsonify({
+            'count': 500000,
+            'data': people
+        })
+
+    except Exception as e:
+        return str(e), 500
